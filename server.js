9708a8c67201bd4de3bc1328f5c4109efaaebd2c
Evan Cheng <evanchengdesign@gmail.com>
2024-04-03 23:47:22 -0400

    add api conneciton and call for /movie

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const axios = require('axios'); 
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
@@ -15,6 +16,11 @@ class Forecast {
 }
 
 //http://localhost:3000/weather?lat=47.6038321&lon=-122.330062&searchQuery=Seattle
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Server! Use /movies or /weather to get started.');
+});
+
 
 app.get('/weather', async (req, res) => {
   try {
@@ -39,9 +45,33 @@ app.get('/weather', async (req, res) => {
   }
 });
 
-// app.get('/', (request, reply) => {
-//     reply.send(`Hello, this is your Express server!`);
-// });
+//http://localhost:3000/movies?location=seattle (/movies is the route) ? = (key value pairs) 
+app.get('/movies', async (req, res) => {
+  const { location } = req.query; // Entire url is the query. Anything after the ? are the variables/parameters 
+  if (!location) {
+    return res.status(400).send({ error: 'Location is required' });
+  }
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${location}`;
+
+  try {
+    const response = await axios.get(url);
+    const movies = response.data.results.map(movie => ({
+      title: movie.title,
+      overview: movie.overview,
+      average_votes: movie.vote_average.toString(),
+      total_votes: movie.vote_count.toString(),
+      image_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      popularity: movie.popularity.toString(),
+      released_on: movie.release_date
+    }));
+    res.json(movies);
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+    res.status(500).json({ error: 'Failed to fetch movies' });
+  }
+});
+
 
 async function getLocation(request, reply) {
 
